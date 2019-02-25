dee6b9fcd5c884edd7b8248cb095ebee4baf9d7f
Kanzaki-Morrison <celina.kanzakimorris@uleth.ca>
2019-02-25 16:47:04 -0700

    added images to the first and second blocks and an onclick to the first

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var l1 = '"There is a Pleasure in the Pathless woods,"';
+var l2 = '"There is a rapture on the lonley shore,"';
+var l3 = '"There is society, where none intrudes"';
+var l4 = '"By the deep sea, and music in its roar:"';
+var l5 = '"I love not man the less, but nature more"';
+
+document.getElementById("clickMe").onclick = function () {
+    Line1()
+};
+
+
+function Line1() {
+    document.getElementById("clickMe").innerHTML = '<h3>"There is a rapture on the lonley shore,"</h3>';
+    document.getElementById("backgroundImage").style.backgroundImage = "url('images/sea-1031262_1920.jpg')";
+}
+
+function Line2() {
+    document.getElementById
+    document.getElementById("backgroundImage").style.backgroundImage = "url('images/nature-3048072_1920.jpg')";
+}
+
+

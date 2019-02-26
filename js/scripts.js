10efb8e7b286d009f74c1854744e284733425a0d
Kanzaki-Morrison <celina.kanzakimorris@uleth.ca>
2019-02-25 17:08:41 -0700

    made it so that when you click the word the title block dissapears

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
-var l1 = '"There is a Pleasure in the Pathless woods,"';
-var l2 = '"There is a rapture on the lonley shore,"';
+var l1 = document.getElementById("l1");
+
+var l2 = document.getElementById("l2");
 var l3 = '"There is society, where none intrudes"';
 var l4 = '"By the deep sea, and music in its roar:"';
 var l5 = '"I love not man the less, but nature more"';
+var title = document.getElementById("title");
+
 
 document.getElementById("clickMe").onclick = function () {
-    Line1()
+    Line1();
+    title.style.display = "none";
 };
 
 
 function Line1() {
-    document.getElementById("clickMe").innerHTML = '<h3>"There is a rapture on the lonley shore,"</h3>';
-    document.getElementById("backgroundImage").style.backgroundImage = "url('images/sea-1031262_1920.jpg')";
+    l1.innerHTML = '"There is a Pleasure in the Pathless woods,"';
+    document.getElementById("backgroundImage").style.backgroundImage = "url('images/tree-3207716_1920.jpg')";
 }
 
 function Line2() {
-    document.getElementById
-    document.getElementById("backgroundImage").style.backgroundImage = "url('images/nature-3048072_1920.jpg')";
+    l2.innerHTML = '"There is a rapture on the lonley shore,"';
+    document.getElementById("backgroundImage").style.backgroundImage = "url('images/sea-1031262_1920.jpg')";
 }
 
 
